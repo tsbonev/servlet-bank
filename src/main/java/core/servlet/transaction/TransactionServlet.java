7d58768d71298b6a7c536e701c34994683cb6c39
Tsvetozar Bonev <tsbonev@gmail.com>
2018-07-04 13:38:45 +0300

    Clean up transaction servlet

--- src/main/java/core/servlet/transaction/TransactionServlet.java
+++ src/main/java/core/servlet/transaction/TransactionServlet.java
@@ -85,31 +85,59 @@ public class TransactionServlet extends HttpServlet {
 
         setConnection(transactionRepository, userRepository);
 
-        LoginSession session = (LoginSession) req.getSession().getAttribute("authorized");
+        String amountString = req.getParameter("amount");
 
-        double amount = Double.parseDouble(req.getParameter("amount").replace(",", "."));
-        Transaction.Operation operation = Transaction.Operation.valueOf(req.getParameter("action").toUpperCase());
+        if(!validateAmountFormat(amountString, req, resp)) return;
 
-        if(amount > maxAmount
-                || amount <= 0){
-            page.redirectTo("/account", resp, req,
-                    "errorMessage", "Transactions of that size are not permitted!");
-            return;
-        }
+        double amount = parseAmount(amountString);
+        Transaction.Operation operation = getOperation(req);
 
-        String amountToString = Double.toString(amount);
-
-        if(!amountToString.matches(wholeNumber)
-            && !amountToString.matches(fraction)
-                && !amountToString.matches(trailingZeroFraction)
-                && !amountToString.matches(leadingZeroFraction)
-                ){
-            page.redirectTo("/account", resp, req,
-                    "errorMessage", "Transaction amount format invalid!");
-            return;
-        }
+        if(!validateAmountSize(amount, req, resp)) return;
 
         String username = req.getParameter("username");
+
+        if(!validateUser(resp, req, username)) return;
+
+        Transaction transaction = setUpTransaction(amount, operation, username);
+
+        transactionRepository.save(transaction);
+
+        page.redirectTo("/account?username=" + username, resp, req,
+                "successMessage", "Transaction successful!");
+    }
+
+    /**
+     * Creates a transaction from a given amount, operation
+     * and username.
+     *
+     * @param amount for the transaction
+     * @param operation of the transaction
+     * @param username of the transaction initiator
+     * @return the built transaction
+     */
+    private Transaction setUpTransaction(double amount, Transaction.Operation operation, String username){
+        Transaction transaction = new Transaction();
+        transaction.setDate(Date.valueOf(LocalDate.now()));
+        transaction.setAmount(amount);
+        transaction.setOperation(operation);
+        transaction.setUserId(userRepository.getByUsername(username).getId());
+        return transaction;
+    }
+
+    /**
+     * Validates whether the user that started
+     * a transaction is the user the transaction
+     * is aimed at or is the admin.
+     *
+     * @param resp servlet response
+     * @param req servlet request
+     * @param username to check
+     * @return result of the check
+     * @throws IOException
+     */
+    private boolean validateUser(HttpServletResponse resp, HttpServletRequest req, String username) throws IOException {
+
+        LoginSession session = (LoginSession) req.getSession().getAttribute("authorized");
 
         if((!session.getUsername().equalsIgnoreCase("admin")
                 && !username.equalsIgnoreCase(session.getUsername()))
@@ -117,20 +145,73 @@ public class TransactionServlet extends HttpServlet {
                 ){
             page.redirectTo("/account", resp, req,
                     "errorMessage", "Action not permitted!");
-            return;
+            return false;
         }
-
-        Transaction transaction = new Transaction();
-        transaction.setDate(Date.valueOf(LocalDate.now()));
-        transaction.setAmount(amount);
-        transaction.setOperation(operation);
-        transaction.setUserId(userRepository.getByUsername(username).getId());
-
-        transactionRepository.save(transaction);
-
-        page.redirectTo("/account?username=" + username, resp, req,
-                "successMessage", "Transaction successful!");
-
-
+        return true;
     }
+
+    /**
+     * Validates whether the string amount passed
+     * is in the correct format.
+     *
+     * @param amount passed as string
+     * @param req servlet request
+     * @param resp servlet response
+     * @return result of the validation
+     * @throws IOException
+     */
+    private boolean validateAmountFormat(String amount,
+                                         HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if(!amount.matches(wholeNumber)
+                && !amount.matches(fraction)
+                && !amount.matches(trailingZeroFraction)
+                && !amount.matches(leadingZeroFraction)
+                ){
+            page.redirectTo("/account", resp, req,
+                    "errorMessage", "Transaction amount format invalid!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Validates whether the double amount exceeds
+     * the max amount permitted.
+     *
+     * @param amount to check
+     * @param req servlet request
+     * @param resp servlet response
+     * @return the result of the check
+     * @throws IOException
+     */
+    private boolean validateAmountSize(double amount, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if(amount > maxAmount
+                || amount <= 0){
+            page.redirectTo("/account", resp, req,
+                    "errorMessage", "Transactions of that size are not permitted!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Parses a string into a double.
+     *
+     * @param amount string to be parsed
+     * @return parsed double
+     */
+    private double parseAmount(String amount){
+        return Double.parseDouble(amount.replace(",", "."));
+    }
+
+    /**
+     * Parses a string to an operation.
+     *
+     * @param req servlet request
+     * @return parsed operation
+     */
+    private Transaction.Operation getOperation(HttpServletRequest req){
+        return Transaction.Operation.valueOf(req.getParameter("action").toUpperCase());
+    }
+
 }
