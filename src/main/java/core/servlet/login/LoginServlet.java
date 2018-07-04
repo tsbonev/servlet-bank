cc2c501278f26b445ae905d7daf9ea5df786398d
Tsvetozar Bonev <tsbonev@gmail.com>
2018-07-04 11:52:36 +0300

    Clean up login servlet

--- src/main/java/core/servlet/login/LoginServlet.java
+++ src/main/java/core/servlet/login/LoginServlet.java
@@ -69,35 +69,86 @@ public class LoginServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        if(StringUtils.isEmpty(username) || StringUtils.isEmpty(password)){
-
-            page.redirectTo("/login", resp, req,
-                    "errorMessage", "Something went wrong!");
-            return;
-
-        }
+        if(!validateUserForm(username, password, req, resp)) return;
 
         user.setUsername(username);
         user.setPassword(password);
 
-        boolean isInSystem = repo.checkPassword(user);
+        if(userIsInSystem(user, resp, req)){
 
-        if(isInSystem){
-
-            if(!UserCounter.getInstance().userIsLoggedIn(user.getUsername())){
-                UserCounter.getInstance().addUserToCount(user.getUsername());
-            }
-
-            LoginSession session = new LoginSession(username, true);
-            req.getSession().setAttribute("authorized", session);
+            loginUserIntoSession(user, req, resp);
 
             page.redirectTo("/home", resp, req,
                     "successMessage", "Successfully logged in!");
         }
-        else {
-            page.redirectTo("/login", resp, req,
-                    "errorMessage", "User not registered!");
+    }
+
+    /**
+     * Adds the user to the session
+     * and starts up the authorized session attribute;
+     * adds the username to the user counter if not
+     * already present.
+     *
+     * @param user to log into session
+     * @param req servlet request
+     * @param resp servlet response
+     */
+    private void loginUserIntoSession(User user, HttpServletRequest req, HttpServletResponse resp){
+
+        UserCounter counter = UserCounter.getInstance();
+
+        if(!counter.userIsLoggedIn(user.getUsername())){
+            counter.addUserToCount(user.getUsername());
         }
 
+        LoginSession session = new LoginSession(user.getUsername(), true);
+        req.getSession().setAttribute("authorized", session);
+
     }
+
+    /**
+     * Checks if the username is in the database
+     * and if it is redirects to the login page with
+     * an error.
+     *
+     * @param user to check
+     * @param resp servlet response
+     * @param req servlet request
+     * @return result of the check
+     * @throws IOException
+     */
+    private boolean userIsInSystem(User user, HttpServletResponse resp, HttpServletRequest req) throws IOException {
+
+        if(!repo.checkPassword(user)){
+            page.redirectTo("/login", resp, req,
+                    "errorMessage", "User not registered!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the form was sent empty
+     * and redirects to an error page if it is.
+     *
+     * @param username to check
+     * @param password to check
+     * @param req servlet request
+     * @param resp servlet request
+     * @return result of the check
+     * @throws IOException
+     */
+    private boolean validateUserForm(String username, String password,
+                                     HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        if(StringUtils.isEmpty(username) || StringUtils.isEmpty(password)){
+
+            page.redirectTo("/login", resp, req,
+                    "errorMessage", "Something went wrong!");
+            return false;
+
+        }
+        return true;
+    }
+
 }
